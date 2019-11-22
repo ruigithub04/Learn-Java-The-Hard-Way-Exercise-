8274db4afdc99010aa739e1ec115344750a237c0
Rui <57881017+Ruizapratama@users.noreply.github.com>
2019-11-22 10:03:04 +0700

    Upload Manually from my computer

---
+++ Exercise2 - More Printing/Drill2.java
@@ -0,0 +1,20 @@
+public class Drill2 {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|     SHELL BUAHBATU     |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   2019-10-01 11:53PM   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   Gallons: 4,5 Gallons |" );
+		System.out.println( "|Price/Gallon: Rp 11,379 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|Fuel Total: Rp 51,205.5 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+
+		/* Rizza Pratama Putra (RUI)
+		Tanggal 01 Oktober 2019
+		Bootcamp G-13 */
+	}
+}
---
+++ Exercise2 - More Printing/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      CORNER STORE      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   2015-03-29 04:38PM   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   Gallons:    10.870   |" );
+		System.out.println( "|  Price/Gallon: $ 2.089 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   Fuel Total: $ 22.71  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
