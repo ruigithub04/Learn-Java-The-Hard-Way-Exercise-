8a85eebfaf404c1a10a16db2554d52536c876094
Rui <57881017+Ruizapratama@users.noreply.github.com>
2019-11-22 10:02:29 +0700

    Upload Manually from my computer

---
+++ Exercise1 - An Important Message/Drill1.java
@@ -0,0 +1,10 @@
+public class Drill1 {
+	public static void main(String[] args) {
+		System.out.println( "I, Rizza Pratama Putra, am determined to learn how to code. " );
+		System.out.println( "Today's date is Tuesday, October 01, 2019");
+
+		/* Rizza Pratama Putra (RUI)
+		Tanggal 01 Oktober 2019
+		Bootcamp G-13 */
+	}
+}
---
+++ Exercise1 - An Important Message/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main(String[] args) {
+		System.out.println( "I am determined to learn how to code. " );
+		System.out.println( "Today's date is");
+	}
+}
