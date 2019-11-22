358d8a620d1d1973a2f25745e2f07d10c3cffca3
Rui <57881017+Ruizapratama@users.noreply.github.com>
2019-11-22 10:03:25 +0700

    Upload Manually from my computer

---
+++ Exercise3 - Printing Choices/Drill3.java
@@ -0,0 +1,26 @@
+public class Drill3 {
+	public static void main( String[] args ){
+		System.out.println( "Alpha" );
+		System.out.println( "Bravo" );
+
+		System.out.println( "Charlie" );
+		System.out.println( "Delta" );
+		System.out.println("");
+
+		System.out.print("Echo");
+		System.out.print("Foxtrot");
+
+		System.out.println( "Golf" );
+		System.out.print("Hotel");
+		System.out.println();
+		System.out.println( "India" );
+
+		System.out.println();
+		System.out.println( "This" + " " + "is" + " " + "a" + " test." );
+		System.out.println( "I" + " " + "am Learning" + " " + "Java the" + " Hard Way!" );
+
+		/* Rizza Pratama Putra (RUI)
+		Tanggal 01 Oktober 2019
+		Bootcamp G-13 */
+	}
+}
---
+++ Exercise3 - Printing Choices/PrintingChoices.java
@@ -0,0 +1,21 @@
+public class PrintingChoices {
+	public static void main( String[] args ){
+		System.out.println( "Alpha" );
+		System.out.println( "Bravo" );
+
+		System.out.println( "Charlie" );
+		System.out.println( "Delta" );
+		System.out.println();
+
+		System.out.print( "Echo" );
+		System.out.print( "Foxtrot" );
+
+		System.out.println( "Golf" );
+		System.out.print( "Hotel" );
+		System.out.println();
+		System.out.println( "India" );
+
+		System.out.println();
+		System.out.println( "This" + " " + "is" + " " + "a" + " test." );
+	}
+}
