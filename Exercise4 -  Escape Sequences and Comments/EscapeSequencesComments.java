b611b65bb285624183eca60923026aa870c1c72a
Rui <57881017+Ruizapratama@users.noreply.github.com>
2019-11-22 10:03:50 +0700

    Upload Manually from my computer

---
+++ Exercise4 -  Escape Sequences and Comments/EscapeSequencesComments.java
@@ -0,0 +1,25 @@
+public class EscapeSequencesComments {
+	public static void main( String[] args ) {
+		// This exercise demonstrates escape sequences & comments (like these)!
+		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
+		System.out.print( "\tLearn Java the \"Hard\" Way!\n" );
+		//System.out.frimp( "Learn Java the Hard Way" );
+		System.out.print( "Hello\n" ); //This line prints Hello.
+		System.out.print( "Jello\by\n" ); //This line prints Jelly.
+		/* The quick brown fox jumped over a lazy dog.
+		Quick wafting zephyrs vex bold Jim. */
+		System.out /* testing */ .println ("Hard to believe, eh?" );
+		System.out.println( "Surprised? /* abcde */ Or what did you expect ?" );
+		System.out./*println*/( "\\ // -=- \\ //");
+		System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); // it takes 2 to make 1
+		System.out.print( "I hope you understand \"escape sequences\" now. \n" );
+		// and comments. :)
+
+		/* Apabila menambahkan comment di tengah syntax, maka akan terjadi error
+		dan tidak akan bisa di compile*/
+
+		/* Rizza Pratama Putra (RUI)
+		Tanggal 01 Oktober 2019
+		Bootcamp G-13 */
+	}
+}
---
+++ Exercise4 -  Escape Sequences and Comments/PrintingChoicesEscapes.java
@@ -0,0 +1,15 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		System.out.println( 
+			"Alpha" + "\nBravo" + "\nCharlie" + "\nDelta" + 
+			" \n " + 
+			"\nEcho" + "Foxtrot" + "Golf" +
+			"\nHotel" + "\nIndia" + 
+			" \n " + 
+			"\nThis" + " " + "is" + " " + "a" + " test.");
+
+		/* Rizza Pratama Putra (RUI)
+		Tanggal 01 Oktober 2019
+		Bootcamp G-13 */
+	}
+}
