5b1a65c1cd7f4567ac1f0b58e90dc15811976f50
Rui <57881017+Ruizapratama@users.noreply.github.com>
2019-11-28 09:04:07 +0700

    Upload Manually from My Computer

---
+++ Exercise5 - Saving Information in Variables/CreatingVariables.java
@@ -0,0 +1,26 @@
+public class CreatingVariables{
+	public static void main ( String[] args) {
+		int x, y, age;
+		double seconds, e, checking;
+		String firstName, lastName, title;
+
+		x = 10;
+		y = 400;
+		age = 39;
+
+		seconds = 4.71;
+		e = 2.771828182845904523536;
+		checking = 1.89;
+
+		firstName = "Graham";
+		lastName = "Mitchell";
+		title = "Mr.";
+
+   System.out.println( "The variable x contains " + x );
+   System.out.println( "The value " + y + " is stored in the variable y." );
+   System.out.println( "The experiment took " + seconds + " seconds." );
+   System.out.println( "A favorite irrational # is Euler's number: " + e );
+   System.out.println( "Hopefully you have more than $" + checking + "!" );
+   System.out.println( "My name's " + title + " " + firstName + lastName );
+	}
+}
---
+++ Exercise5 - Saving Information in Variables/Drill5.java
@@ -0,0 +1,22 @@
+public class Drill5{
+	public static void main ( String[] args) {
+		int umur;
+		double tinggi;
+		String nama, generasi;
+
+		umur = 25;
+
+		tinggi = 163;
+
+		nama = "Rui";
+		generasi = " Gen-13";
+
+   System.out.println( "Nama:" + nama );
+   System.out.println( "Generasi:" + generasi );
+   System.out.println( "Usia:" + umur + " tahun" );
+   System.out.println( "Tinggi:" + tinggi + " cm");
+   /* Rizza Pratama Putra (RUI)
+		Tanggal 01 Oktober 2019
+		Bootcamp G-13 */
+	}
+}
